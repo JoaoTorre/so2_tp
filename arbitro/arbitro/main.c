e636dec99e3535ed20b57091186e1a9f16941639
nunomauricio <nunomauricio07@gmail.com>
2025-04-13 19:40:10 +0100

    Teste Github

---
+++ arbitro/arbitro/main.c
@@ -0,0 +1,19 @@
+#include <windows.h>
+#include <tchar.h>
+#include <io.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <time.h>
+
+
+int _tmain(int argc, TCHAR* argv[]) {
+
+#ifdef UNICODE 
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+	_setmode(_fileno(stderr), _O_WTEXT);
+#endif
+	_tprintf(_T("Ola Mundo\n"));
+
+	return 0;
+}
